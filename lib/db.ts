7809f090dbb594b3185f30c094faca583d24580c
Yasmin Elmahdi <elmahdiyasmin@.com>
2024-01-25 21:06:46 +0300

    deployment fix2

--- lib/db.ts
+++ lib/db.ts
@@ -7,18 +7,13 @@ declare global {
 // Use an SQLite-specific connection string for local development
 const sqliteConnectionString = "file:./data/dev.db"; // Adjust the path as needed
 
-export const db =
-  globalThis.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url:
-          process.env.NODE_ENV === "production"
-            ? process.env.DATABASE_URL
-            : sqliteConnectionString,
-      },
+export const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: "file:./data/dev.db",
     },
-  });
+  },
+});
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
