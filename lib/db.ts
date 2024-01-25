579ce1038a1bfe201d890458d6b98716597ca38e
Yasmin Elmahdi <elmahdiyasmin@.com>
2024-01-25 23:30:26 +0300

    Local db fixes 1

--- lib/db.ts
+++ lib/db.ts
@@ -4,25 +4,6 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-// Use an SQLite-specific connection string for local development
-const sqliteConnectionString = "file:./data/dev.db"; // Adjust the path as needed
-
-export const db = new PrismaClient({
-  datasources: {
-    db: {
-      url: "file:./data/dev.db",
-    },
-  },
-});
+export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
-
-// import { PrismaClient } from "@prisma/client";
-
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-
-// export const db = globalThis.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
